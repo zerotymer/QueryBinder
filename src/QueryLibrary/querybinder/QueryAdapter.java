a810da06a639c22265c7320209636b86f759ac6a
Shin Hyunjin <zerotymer@gmail.com>
2022-05-20 10:02:48 +0900

    2022-05-20 오전 10:02:31  | static 메서드, reuqest 인터페이스 추가

--- src/QueryLibrary/querybinder/QueryAdapter.java
+++ src/QueryLibrary/querybinder/QueryAdapter.java
@@ -24,11 +24,8 @@ public class QueryAdapter {
     /// FIELDs
 
     /// CONSTRUCTORs
-    public QueryAdapter() {
-    }
 
     /// METHODs
-    // Getters & Setters
 
     /**
      * 쿼리 요청을 실행하기 위한 메소드
@@ -133,35 +130,108 @@ public class QueryAdapter {
         return null;
     }
 
-    /**
-     * Json을 Map으로 변환한다.
-     * @param json
-     * @return
-     */
-    public static Map<?, ?> JsonToMap(String json) {
-        return new JSONObject(json).toMap();
-    }
 
+    /// Static
     /**
-     * Json을 Array로 변환한다.
-     * @param json
+     * 쿼리 요청을 실행하기 위한 메소드
+     * @param query 쿼리 문자열(url)
+     * @param method 쿼리 메소드(GET, POST, PUT, DELETE)
      * @return
+     * @throws MalformedURLException
      */
-    public static List<?> jsonToArray(String json) {
-        ArrayList<Map<?, ?>> list = new ArrayList<>();
+    public static String staticRequest(String query, HttpRequestMethods method)
+            throws MalformedURLException {
+        // URL check
+        if (query.isEmpty()) throw new MalformedURLException("URL is empty");
+        URL url = new URL(query);
+
         try {
-            JSONArray jsonArray = new JSONArray(json);
-            jsonArray.forEach(jsonObject -> list.add(new JSONObject(jsonObject.toString()).toMap()));
-        } catch (JSONException e) {
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod(method.toString());
+
+            // Header 설정
+
+            // Response
+            int responseCode = conn.getResponseCode();
+            BufferedReader br = new BufferedReader(
+                    new InputStreamReader(
+                            responseCode == HttpURLConnection.HTTP_OK ? conn.getInputStream() : conn.getErrorStream()));
+
+            // TO STRING
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+            while ((inputLine = br.readLine()) != null) {
+                response.append(inputLine);
+            }
+            br.close();
+
+            return response.toString();
+
+        } catch (IOException e) {
             e.printStackTrace();
-            System.err.println(QueryAdapter.class.getName() + " : " + e.getMessage());
-            System.err.println("STRING DATA ========================================");
-            System.err.println(json);
+            System.err.println("Query failed");
         }
-        return list;
+
+        return null;
     }
 
+    /**
+     * 쿼리 요청을 실행하기 위한 메소드
+     * @param map
+     * @param method
+     * @return
+     * @throws MalformedURLException
+     */
+    public static String staticRequest(QueryMap map, HttpRequestMethods method)
+            throws MalformedURLException {
+        // URL check
+        if (map.getUrl().isEmpty()) throw new MalformedURLException("URL is empty");
+        URL url = new URL(map.toQueryString());
+
+        try {
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod(method.toString());
+
+            // Header 설정
+            Map<?, ?> header = map.getHeader();
+            if (header != null) {
+                header.keySet().forEach(key -> {
+                    conn.setRequestProperty(key.toString(), (String) header.get(key));
+                });
+            }
+
+            // Body 설정
 
 
+            // Post 전송
+            if (method == HttpRequestMethods.POST) {
+                conn.setDoOutput(true);
+                OutputStream os = conn.getOutputStream();
+                os.write(map.getBytes());
+                os.flush();
+            }
 
+            // Response
+            int responseCode = conn.getResponseCode();
+            BufferedReader br = new BufferedReader(
+                    new InputStreamReader(
+                            responseCode == HttpURLConnection.HTTP_OK ? conn.getInputStream() : conn.getErrorStream()));
+
+            // TO STRING
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+            while ((inputLine = br.readLine()) != null) {
+                response.append(inputLine);
+            }
+            br.close();
+
+            return response.toString();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.err.println("Query failed");
+        }
+
+        return null;
+    }
 }
--- src/QueryLibrary/querybinder/QueryRequestable.java
+++ src/QueryLibrary/querybinder/QueryRequestable.java
@@ -1,7 +1,10 @@
 package querylibrary.querybinder;
 
+import querylibrary.querybinder.Request.HttpRequestMethods;
+
 import java.io.UnsupportedEncodingException;
 import java.lang.reflect.InvocationTargetException;
+import java.net.MalformedURLException;
 
 /**
  * QuaryMap을 이용하여 요청가능
@@ -13,5 +16,11 @@ public interface QueryRequestable {
     default QueryMap getQueryMap()
             throws InvocationTargetException, IllegalAccessException, UnsupportedEncodingException {
         return new QueryMap(this);
+    }
+
+    default String request(HttpRequestMethods method)
+            throws UnsupportedEncodingException, InvocationTargetException, IllegalAccessException, MalformedURLException {
+        QueryMap map = new QueryMap(this);
+        return QueryAdapter.staticRequest(map, method);
     }
 }
