e89517630e8964016e92d5f280616f7e83d244af
Shin Hyunjin <zerotymer@gmail.com>
2022-05-20 10:18:46 +0900

    2022-05-20 오전 10:18:35 | 필드 추가

--- src/QueryLibrary/querybinder/QueryRequestable.java
+++ src/QueryLibrary/querybinder/QueryRequestable.java
@@ -13,12 +13,15 @@ import java.net.MalformedURLException;
  */
 
 public interface QueryRequestable {
+    /// FIELDs
+    HttpRequestMethods method = HttpRequestMethods.GET;
+
     default QueryMap getQueryMap()
             throws InvocationTargetException, IllegalAccessException, UnsupportedEncodingException {
         return new QueryMap(this);
     }
 
-    default String request(HttpRequestMethods method)
+    default String request()
             throws UnsupportedEncodingException, InvocationTargetException, IllegalAccessException, MalformedURLException {
         QueryMap map = new QueryMap(this);
         return QueryAdapter.staticRequest(map, method);
